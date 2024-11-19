5e66d454dc14818a8b3c0f893687f5917b89674c
Stoyan Chompov <stoqnchompov@gmail.com>
2024-11-19 16:11:09 +0200

    create java project - server

---
+++ AcademicEmploymentSystem/src/test/java/bg/example/academicemploymentsystem/AcademicEmploymentSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package bg.example.academicemploymentsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AcademicEmploymentSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
